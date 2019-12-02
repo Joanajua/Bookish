5e3e32e0fa9a6c1078adbf358e117e18a384e0e0
Joanajua <J.Ayan@ucas.ac.uk>
2019-12-02 15:43:39 +0000

    connectionString to dataBase

--- Bookish.ConsoleApp/Bookish/Program.cs
+++ Bookish.ConsoleApp/Bookish/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
+using Npgsql;
 
 namespace Bookish
 {
@@ -10,6 +13,16 @@ namespace Bookish
     {
         static void Main(string[] args)
         {
+            string connectionString = "Server=127.0.0.1; Port=5432; Database=bookish; User Id=bookish; Password=123";
+            var connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+
+            var books = connection.Query<Book>("SELECT * FROM books");
+
+            Console.ReadLine();
         }
+        
     }
+   
+
 }
